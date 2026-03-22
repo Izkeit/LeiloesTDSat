1b2c352c7152959eef5061b75135f44e4862bbe8
Izkeit <geffvieira@hotmail.com>
2026-03-22 10:55:41 -0300

    Commit #1: botão de salvar cadastrando produto e exibindo mensagem

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,12 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Adm
- */
 
 import java.sql.PreparedStatement;
 import java.sql.Connection;
@@ -23,12 +15,22 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+    conn = new conectaDAO().connectDB();
+    String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
+
+        prep.executeUpdate();
+        JOptionPane.showMessageDialog(null, "Cadastro realizado com sucesso!");
+
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro ao cadastrar: " + e.getMessage());
     }
+}
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
